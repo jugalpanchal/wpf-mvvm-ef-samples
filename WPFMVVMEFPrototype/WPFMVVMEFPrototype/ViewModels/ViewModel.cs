b339b06bab779283003db81d6a5ca46009f809af
Jugal Panchal <jugalpanchal@gmail.com>
2017-03-11 06:01:51 +0530

    Implemented INotifyPropertyChanged

--- WPFMVVMEFPrototype/WPFMVVMEFPrototype/ViewModels/ViewModel.cs
+++ WPFMVVMEFPrototype/WPFMVVMEFPrototype/ViewModels/ViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WPFMVVMEFPrototype.ViewModels
 {
-    class ViewModel
+    abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
