becdb45dd57d3bc1487bdbcc8af3f6580ac5ef83
Jugal Panchal <jugalpanchal@gmail.com>
2017-03-11 05:55:53 +0530

    Launch login view

--- WPFMVVMEFPrototype/WPFMVVMEFPrototype/App.xaml.cs
+++ WPFMVVMEFPrototype/WPFMVVMEFPrototype/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WPFMVVMEFPrototype.ViewModels;
+using WPFMVVMEFPrototype.Views;
 
 namespace WPFMVVMEFPrototype
 {
@@ -13,5 +15,12 @@ namespace WPFMVVMEFPrototype
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var loginView = new LoginView();
+            loginView.DataContext = new LoginViewModel();
+            loginView.Show();
+        }
     }
 }
