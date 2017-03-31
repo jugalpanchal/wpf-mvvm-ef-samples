146161ad2f675908c8f2ff697c61e47a4b19adbd
Jugal Panchal <jugalpanchal@gmail.com>
2017-03-31 09:07:16 +0530

    Added test cases

---
+++ WPFMVVMEFPrototype/WPFMVVMEFPrototypeUnitTest/NavigationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMVVMEFPrototype.ServiceLocator;
+using WPFMVVMEFPrototype.ViewModels;
+
+namespace WPFMVVMEFPrototypeUnitTest
+{
+    public class NavigationServiceMock : INavigationService
+    {
+        public bool ShellActive { get; private set; }
+        public ShellViewModel ShellVM { get; private set; }
+
+        public bool SettingActive { get; private set; }
+
+        public void Shell(ViewModel viewModel)
+        {
+            this.ShellActive = true;
+            this.ShellVM = viewModel as ShellViewModel;
+        }
+
+        public void Setting(ViewModel viewModel)
+        {
+            this.SettingActive = true;
+        }
+    }
+}
---
+++ WPFMVVMEFPrototype/WPFMVVMEFPrototypeUnitTest/NavigationServiceUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFMVVMEFPrototype.ServiceLocator;
+using WPFMVVMEFPrototype.ViewModels;
+using System.Security;
+
+namespace WPFMVVMEFPrototypeUnitTest
+{
+    [TestClass]
+    public class NavigationServiceUnitTest
+    {
+        [TestMethod]
+        public void ShellTest()
+        {
+            NavigationServiceMock navigationService = new NavigationServiceMock();
+            var loginViewModel = new LoginViewModel(navigationService);
+
+            loginViewModel.UserId = "Jugal";
+
+            SecureString secureString = new SecureString();
+            secureString.AppendChar('l');
+            secureString.AppendChar('a');
+            secureString.AppendChar('g');
+            secureString.AppendChar('u');
+            secureString.AppendChar('j');
+
+            loginViewModel.LoginCommand.Execute(secureString);
+
+            Assert.IsTrue(navigationService.ShellActive, "Shell is not active.");
+        }
+
+        [TestMethod]
+        public void SettingTest()
+        {
+            NavigationServiceMock navigationService = new NavigationServiceMock();
+            var loginViewModel = new LoginViewModel(navigationService);
+
+            loginViewModel.UserId = "Jugal";
+
+            SecureString secureString = new SecureString();
+            secureString.AppendChar('l');
+            secureString.AppendChar('a');
+            secureString.AppendChar('g');
+            secureString.AppendChar('u');
+            secureString.AppendChar('j');
+
+            loginViewModel.LoginCommand.Execute(secureString);
+            Assert.IsTrue(navigationService.ShellActive, "Shell is not active.");
+
+            navigationService.ShellVM.SettingCommand.Execute(null);
+            Assert.IsTrue(navigationService.SettingActive, "Setting is not active.");
+        }
+    }
+}
